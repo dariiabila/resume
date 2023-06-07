5a0b4719028e6a7136f529596c24ea2eb19ad5ab
dariiabila <dasabila01@gmail.com>
2023-06-07 19:22:57 +0300

    updated pages index8

--- src/route/index.js
+++ src/route/index.js
@@ -3430,5 +3430,15 @@ router.get('/bootstrap_style', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/index', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('index8', {
+    layout: 'index',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // Підключаємо роутер до бек-енду
 module.exports = router
